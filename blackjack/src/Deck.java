e5a6a9e979889f8d554bddb2c608c0472946652b
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-18 10:59:56 +0200

    add methods to deck

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -5,9 +5,12 @@ public class Deck {
   String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
   String[] colors = {"Clubs", "Diamonds", "Hearts", "Spades"};
   List<Card> cardsInDeck;
+  int[] nrOfColors = {0, 0, 0, 0};
 
   public Deck(int numberOfCards) {
     cardsInDeck = new ArrayList<>();
+    this.fillDeck(numberOfCards);
+    this.shuffle();
   }
 
   public Deck() {
@@ -18,6 +21,7 @@ public class Deck {
     int fillTill = Math.min(numberOfCards, 52);
     for (int i = 0; i < numberOfCards; i++) {
       cardsInDeck.add(new Card(colors[i % 4], values[i / 4]));
+      nrOfColors[i % 4] += 1;
     }
   }
 
@@ -29,5 +33,24 @@ public class Deck {
       cardsInDeck.remove(pick);
     }
     cardsInDeck = buffer;
+  }
+
+  public Card draw() {
+    try {
+      Card bufferCard = cardsInDeck.get(0);
+      cardsInDeck.remove(0);
+      return bufferCard;
+    }
+    catch (Exception e) {
+      System.out.println("Deck is empty");
+      return null;
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    // 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+    return this.cardsInDeck.size() + " cards - " + nrOfColors[0] + "Clubs, " + nrOfColors[1] + "Diamonds, " + nrOfColors[2] + "Hearts, " + nrOfColors[3] + "Spades";
   }
 }

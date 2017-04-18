7ac84a105ea6c0f0f36071caf785b6de3724aa7e
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-18 11:06:35 +0200

    finetune String format of deck representation and fix number of colors available in the deck

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -17,9 +17,28 @@ public class Deck {
     cardsInDeck = new ArrayList<>();
   }
 
+  public void countColors(List<Card> cardsToCount) {
+    int[] colorBuffer = {0, 0, 0, 0};
+    for(Card c : cardsToCount) {
+      if (c.color.equals("Clubs")) {
+        colorBuffer[0] += 1;
+      }
+      else if (c.color.equals("Diamonds")) {
+        colorBuffer[1] += 1;
+      }
+      else if (c.color.equals("Hearts")) {
+        colorBuffer[2] += 1;
+      }
+      else {
+        colorBuffer[3] += 1;
+      }
+    }
+    nrOfColors = colorBuffer;
+  }
+
   public void fillDeck(int numberOfCards) {
     int fillTill = Math.min(numberOfCards, 52);
-    for (int i = 0; i < numberOfCards; i++) {
+    for (int i = 0; i < fillTill; i++) {
       cardsInDeck.add(new Card(colors[i % 4], values[i / 4]));
       nrOfColors[i % 4] += 1;
     }
@@ -39,6 +58,7 @@ public class Deck {
     try {
       Card bufferCard = cardsInDeck.get(0);
       cardsInDeck.remove(0);
+      this.countColors(cardsInDeck);
       return bufferCard;
     }
     catch (Exception e) {
@@ -50,7 +70,6 @@ public class Deck {
   @Override
   public String toString()
   {
-    // 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-    return this.cardsInDeck.size() + " cards - " + nrOfColors[0] + "Clubs, " + nrOfColors[1] + "Diamonds, " + nrOfColors[2] + "Hearts, " + nrOfColors[3] + "Spades";
+    return this.cardsInDeck.size() + " cards - " + nrOfColors[0] + " Clubs, " + nrOfColors[1] + " Diamonds, " + nrOfColors[2] + " Hearts, " + nrOfColors[3] + " Spades";
   }
 }

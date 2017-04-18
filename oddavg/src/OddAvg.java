e7e9a9821ed6498fb324b787ca0db59f0c956420
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-18 09:26:48 +0200

    add oddAvergae method that counts the average of odd numbers in a list

--- oddavg/src/OddAvg.java
+++ oddavg/src/OddAvg.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class OddAvg {
   public static void main(String[] args) {
     // Create a function called `oddAverage` that takes a list of numbers as parameter
@@ -5,5 +7,15 @@ public class OddAvg {
     // Create basic unit tests for it with at least 3 different test cases
   }
 
-  
+  public double oddAverage (List<Double> averageOf) {
+    int counter = 0;
+    double sum = 0;
+    for (double num : averageOf) {
+      if (num % 2 == 1) {
+        counter += 1;
+        sum += num;
+      }
+    }
+    return sum / counter;
+  }
 }
